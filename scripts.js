9a232b6b98aaaf1c822a08978e1dbdda6210f185
Clive Tortshwar <39255598+tortshwar@users.noreply.github.com>
2018-08-12 04:37:39 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,6 @@
 const app = document.getElementById('root');
 
-const logo = document.createElement('img');
-logo.src = 'logo.png';
-
 const container = document.createElement('div');
 container.setAttribute('class', 'container');
 
-app.appendChild(logo);
 app.appendChild(container);
